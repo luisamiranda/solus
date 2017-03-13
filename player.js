4503dda98087daeb5c570c159448adc143cb6d4d
Luisa de Miranda <lemiranda@gmail.com>
2017-03-13 12:30:11 -0400

    now with mood music

--- player.js
+++ player.js
@@ -49,109 +49,58 @@ $('.playlist li').removeClass('active');
 elem.addClass('active');
 }
 function playAudio() {
-19
 song.play();
-20
 tracker.slider("option", "max", song.duration);
-21
 $('.play').addClass('hidden');
-22
 $('.pause').addClass('visible');
-23
 }
-24
 function stopAudio() {
-25
 song.pause();
-26
 $('.play').removeClass('hidden');
-27
 $('.pause').removeClass('visible');
-28
 }
-And then I started to add event handlers to our control buttons. Play / Pause buttons:
 
-01
 // play click
-02
 $('.play').click(function (e) {
-03
 e.preventDefault();
-04
 playAudio();
-05
 });
-06
 // pause click
-07
 $('.pause').click(function (e) {
-08
 e.preventDefault();
-09
 stopAudio();
-10
 });
-In order to turn to another song in the playlist, we have to stop playing a current song, pick a next (or previous) object in the playlist, and re-initialize our Audio element. Forward / Rewind buttons:
-
-01
 // forward click
-02
 $('.fwd').click(function (e) {
-03
 e.preventDefault();
-04
 stopAudio();
-05
 var next = $('.playlist li.active').next();
-06
 if (next.length == 0) {
-07
 next = $('.playlist li:first-child');
-08
 }
-09
 initAudio(next);
-10
 });
-11
 // rewind click
-12
 $('.rew').click(function (e) {
-13
 e.preventDefault();
-14
 stopAudio();
-15
 var prev = $('.playlist li.active').prev();
-16
 if (prev.length == 0) {
-17
 prev = $('.playlist li:last-child');
-18
 }
-19
 initAudio(prev);
-20
 });
-Finally, few functions to handle with the playlist:
-
-01
 // show playlist
-02
 $('.pl').click(function (e) {
-03
 e.preventDefault();
-04
+
 $('.playlist').fadeIn(300);
-05
 });
-06
+
 // playlist elements - click
-07
 $('.playlist li').click(function () {
-08
+
 stopAudio();
-09
+
 initAudio($(this));
-10
 });
--- public/script.js
+++ public/script.js
@@ -4,11 +4,11 @@ var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
 /*--------------- Create Grammar/Vocab ---------------*/
-var greatMoods = ['great', 'I\'m doing great', 'awesome', 'I\'m excited', 'I\'m happy'];
+var greatMoods = ['great', 'I\'m great', 'I feel great', 'awesome', 'I\'m excited', 'I\'m happy'];
 var goodMoods = ['well', 'I\'m well', 'very well, thanks','good', 'I\'m good', 'pretty good'];
 var sadMoods = ['depressed', 'I\'m depressed', 'sad', 'I\'m sad', 'been better', 'I\'ve been better'];
 var tiredMoods = ['tired', 'I\'m tired', 'I\'m so tired', 'exhausted', 'I\'m exhausted', 'sleepy', 'I\'m sleepy'];
-var tenseMoods = ['stressed', 'I\'m stressed', 'anxious', 'I\'m anxious', 'worried', 'I\'m worried'];
+var tenseMoods = ['stressed', 'I\'m stressed', 'anxious', 'I\'m anxious', 'worried', 'I\'m worried', 'frustrated', 'I\'m frustrated'];
 var vague = ['okay', 'I\'m okay', 'fine', 'I\'m fine', 'fine, thank you', 'not bad', 'not so bad', 'not too bad', 'eh', 'meh'];
 
 var grammar = '#JSGF V1.0; grammar colors; public <goodMoods> = ' +
@@ -50,12 +50,14 @@ function say (response) {
 /*--------------- Define Page Outputs ---------------*/
 var diagnostic = document.querySelector('.output');
 var bg = document.querySelector('body');
-var audio = document.getElementById('player')
+var audio = document.getElementById('player');
+//var source = document.getElementById('source');
 bg.style.background = "url(img/solace-default.png) center";
 
 /*--------------- Start Interaction! ---------------*/
 say("How are you? Click to speak.");
-
+audio.src="audio/01 Raga Jogeshwari, Alap 1.mp3";
+audio.load();
 document.body.onclick = function() {
   recognition.start();
   console.log('Ready to receive.');
@@ -74,18 +76,28 @@ recognition.onresult = function(event) {
   if (greatMoods.includes(input)) {
     response = "I'm happy to hear that. I hope you like this!";
     bg.style.background = "url(img/midday-2.jpg)";
+    audio.src = "audio/Its_Not_Night_Its_Space_-_02_-_Starry_Wisdom.mp3";
+    audio.load();
   } else if (goodMoods.includes(input)) {
     response = "I'm glad. Enjoy this painting of a sunrise.";
     bg.style.background = "url(img/sunrise-4.jpg)";
+    audio.src = "audio/02 Miss Modular.mp3";
+    audio.load();
   } else if (sadMoods.includes(input)) {
     response = "I'm sorry. Maybe this will make you feel better.";
     bg.style.background = "url(img/sunrise-1.jpg)";
+    audio.src = "audio/01 Sleepwalker.mp3";
+    audio.load();
   } else if (tiredMoods.includes(input)) {
     response = "Here's something energizing";
     bg.style.background = "url(img/midday-1.jpg)";
+    audio.src = "audio/Iva_Bittova_-_04_-_Improvisation_4.mp3";
+    audio.load();
   } else if (tenseMoods.includes(input)) {
     response = "I'm sorry. Here's something I find calming.";
     bg.style.background = "url(img/dusk-2.jpg)";
+    audio.src = "audio/07 Corcovado.mp3";
+    audio.load();
   } else if (vague.includes(input)) {
     response = "Can you tell me more?";
   }
