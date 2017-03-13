38fb68ddeafc4080f8e4c5717189862001b7a16a
Luisa de Miranda <lemiranda@gmail.com>
2017-03-12 20:19:45 -0400

    fixes port # error

--- start.js
+++ start.js
@@ -9,4 +9,4 @@ module.exports = app
   .use(express.static(__dirname + '/public'))
   .get('/', (req, res, next) => res.render('index'))
   .get('/', (err, req, res, next) => console.error(err))
-  .listen(5050, (req, res, next) => console.log('solus listening 5050'))
+  .listen(process.env.PORT || 5050, (req, res, next) => console.log('solus listening 5050'))
